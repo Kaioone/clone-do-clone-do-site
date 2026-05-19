553ccffc2507999274e3940920d2e9dc1a2a033f
Kaioone <kaioaraujo97@hotmail.com>
2026-05-19 00:06:15 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-do-clone-do-site
+
 Primeiro Curso de Projeto de Front-end
---
+++ pages/Sobre.js
@@ -0,0 +1,6 @@
+function Sobre() {
+  return <h2>Essa é a página "Sobre"</h2>;
+}
+
+export default Sobre;
+/// Ainda nao dei Git add, nem commit, nem push
